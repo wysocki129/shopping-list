fffbfd65f598ef54fdb77d513c19de5317eac33d
Chris Wysocki <christopher.wysocki@gmail.com>
2019-06-04 06:13:04 -0700

    text clears out when entering new list item

--- index.js
+++ index.js
@@ -29,12 +29,14 @@ $('ul').on('click', '.shopping-item-delete', function(event){
 $('ul').on('click', '.shopping-item-toggle', function(event){
     $(this).parents("li").find(".shopping-item").toggleClass("shopping-item__checked");
 });
+
 $('button[type="submit"]').on('click', function(event){
     event.preventDefault();    
     let newText = $('input[name="shopping-list-entry"]').val();
-    $('ul').append('<li><span class="shopping-item">' + newText + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-});
 
+    $('ul').append('<li><span class="shopping-item">' + newText + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    $('input[name="shopping-list-entry"]').val("");
+});
 
 
 
