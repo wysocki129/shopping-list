1f607043be7331168b1618ea8de7a1f6e10d2348
Chris Wysocki <christopher.wysocki@gmail.com>
2019-06-04 05:31:33 -0700

    finished adding new list item functionallity, will test tomorrow berfor pushing final revision

--- index.js
+++ index.js
@@ -6,17 +6,36 @@ function deleteItem(){
     //TODO
 };
 
-//Toggle checked
-$('.shopping-item-toggle' ).on('click', function(event){
+// //Toggle checked
+// $('.shopping-item-toggle' ).on('click', function(event){
 
+//     $(this).parents("li").find(".shopping-item").toggleClass("shopping-item__checked");
+
+// });
+
+
+// //Delete Item
+// $('.shopping-item-delete' ).on('click', function(event){
+
+//     $(this).parents("li").remove();
+
+// })
+
+$('ul').on('click', '.shopping-item-delete', function(event){
+    $(this).parents('li').remove();
+    event.stopPropagation();
+});
+
+$('ul').on('click', '.shopping-item-toggle', function(event){
     $(this).parents("li").find(".shopping-item").toggleClass("shopping-item__checked");
-
+});
+$('button[type="submit"]').on('click', function(event){
+    event.preventDefault();    
+    let newText = $('input[name="shopping-list-entry"]').val();
+    $('ul').append('<li><span class="shopping-item">' + newText + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
 });
 
 
-//Delete Item
-$('.shopping-item-delete' ).on('click', function(event){
 
-    $(this).parents("li").remove();
 
-})
+
